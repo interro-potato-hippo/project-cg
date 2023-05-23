796894bf611a35740bdddfecd2f6f8d804f8b025
Diogo Correia <diogo.t.correia@tecnico.ulisboa.pt>
2023-05-23 12:50:11 +0100

    style: use caps for global constant

--- b-robot/js/main-script.js
+++ b-robot/js/main-script.js
@@ -40,7 +40,7 @@ const GEOMETRY = Object.freeze({
   foreheadHeight: 0.2,
 });
 
-const background = new THREE.Color(0xC0E8EE);
+const BACKGROUND = new THREE.Color(0xc0e8ee);
 
 //////////////////////
 /* GLOBAL VARIABLES */
@@ -56,7 +56,7 @@ function createScene() {
 
   scene = new THREE.Scene();
   scene.add(new THREE.AxisHelper(20));
-  scene.background = background;
+  scene.background = BACKGROUND;
 
   createRobot();
 }
