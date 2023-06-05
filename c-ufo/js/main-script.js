da1f186009ea37ff2fcf153b0b7069bd3309cd45
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-06-05 10:22:32 +0100

    feat(ufo): initialize scene with axis helper

--- c-ufo/js/main-script.js
+++ c-ufo/js/main-script.js
@@ -1,17 +1,53 @@
 'use strict';
 
 //////////////////////
+/* GLOBAL CONSTANTS */
+//////////////////////
+
+const CAMERA_GEOMETRY = Object.freeze({
+  fov: 80,
+  near: 1,
+  far: 1000,
+});
+const ORBITAL_CAMERA = createPerspectiveCamera({ x: -10, y: 20, z: -10 });
+
+//////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+
+let renderer, scene;
+let activeCamera = ORBITAL_CAMERA; // starts as the orbital camera, may change afterwards
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene() {}
+function createScene() {
+  scene = new THREE.Scene();
+  scene.add(new THREE.AxesHelper(20));
+}
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createCameras() {
+  const controls = new THREE.OrbitControls(activeCamera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+  controls.update();
+}
+
+function createPerspectiveCamera({ x = 0, y = 0, z = 0 }) {
+  const aspect = window.innerWidth / window.innerHeight;
+
+  const camera = new THREE.PerspectiveCamera(
+    CAMERA_GEOMETRY.fov,
+    aspect,
+    CAMERA_GEOMETRY.near,
+    CAMERA_GEOMETRY.far
+  );
+  camera.position.set(x, y, z);
+  camera.lookAt(x, y, z);
+  return camera;
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -39,22 +75,43 @@ function update() {}
 /////////////
 /* DISPLAY */
 /////////////
-function render() {}
+function render() {
+  renderer.render(scene, activeCamera);
+}
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
-function init() {}
+function init() {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  createCameras();
+}
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
-function animate() {}
+function animate() {
+  render();
+  requestAnimationFrame(animate);
+}
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() {}
+function onResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  if (window.innerHeight > 0 && window.innerWidth > 0) {
+    activeCamera.aspect = window.innerWidth / window.innerHeight;
+    activeCamera.updateProjectionMatrix();
+  }
+}
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
