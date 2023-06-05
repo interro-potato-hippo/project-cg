3fa9ce909b717ff009e17a3a5279170c1f29ac89
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2023-06-05 10:38:20 +0100

    feat(ufo): initialize scene with axis helper, add terrain plane (#49)

--- c-ufo/js/main-script.js
+++ c-ufo/js/main-script.js
@@ -1,17 +1,61 @@
 'use strict';
 
 //////////////////////
+/* GLOBAL CONSTANTS */
+//////////////////////
+const MATERIAL = Object.freeze({
+  terrain: new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }),
+});
+const GEOMETRY = Object.freeze({
+  terrain: new THREE.PlaneGeometry(100, 100, 100, 100),
+});
+
+const CAMERA_GEOMETRY = Object.freeze({
+  fov: 80,
+  near: 1,
+  far: 1000,
+});
+const ORBITAL_CAMERA = createPerspectiveCamera({ x: -10, y: 20, z: -10 });
+
+//////////////////////
 /* GLOBAL VARIABLES */
 //////////////////////
+
+let renderer, scene;
+let activeCamera = ORBITAL_CAMERA; // starts as the orbital camera, may change afterwards
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
-function createScene() {}
+function createScene() {
+  scene = new THREE.Scene();
+  scene.add(new THREE.AxesHelper(20));
+
+  createTerrain();
+}
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
+function createCameras() {
+  const controls = new THREE.OrbitControls(activeCamera, renderer.domElement);
+  controls.target.set(0, 0, 0);
+  controls.update();
+}
+
+function createPerspectiveCamera({ x = 0, y = 0, z = 0 }) {
+  const aspect = window.innerWidth / window.innerHeight;
+
+  const camera = new THREE.PerspectiveCamera(
+    CAMERA_GEOMETRY.fov,
+    aspect,
+    CAMERA_GEOMETRY.near,
+    CAMERA_GEOMETRY.far
+  );
+  camera.position.set(x, y, z);
+  camera.lookAt(x, y, z);
+  return camera;
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -20,6 +64,11 @@ function createScene() {}
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+function createTerrain() {
+  const plane = new THREE.Mesh(GEOMETRY.terrain, MATERIAL.terrain);
+  plane.rotateX(-Math.PI / 2); // we rotate it so that it is in the xOz plane
+  scene.add(plane);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -39,22 +88,43 @@ function update() {}
 /////////////
 /* DISPLAY */
 /////////////
-function render() {}
+function render() {
+  renderer.render(scene, activeCamera);
+}
 
 ////////////////////////////////
 /* INITIALIZE ANIMATION CYCLE */
 ////////////////////////////////
-function init() {}
+function init() {
+  renderer = new THREE.WebGLRenderer({
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  createCameras();
+}
 
 /////////////////////
 /* ANIMATION CYCLE */
 /////////////////////
-function animate() {}
+function animate() {
+  render();
+  requestAnimationFrame(animate);
+}
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
-function onResize() {}
+function onResize() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  if (window.innerHeight > 0 && window.innerWidth > 0) {
+    activeCamera.aspect = window.innerWidth / window.innerHeight;
+    activeCamera.updateProjectionMatrix();
+  }
+}
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
